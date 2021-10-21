2fb5436f67ea2f5b1efefcb35733704b6b611fc2
0andme <yu00mi97@gmail.com>
2021-10-22 03:12:21 +0900

    feat : promise 객체 출력 및 영화 정보 출력
    1. promise 객체 출력
    2. 영화 정보 출력

---
+++ 영화정보/main.js
@@ -0,0 +1,19 @@
+const url = "https://www.omdbapi.com/?i=tt3896198&apikey=7035c60c"
+
+async function fetchMovie() {
+    const res = await fetch(url)
+    return await res.json()
+}
+
+console.log("Promise 객체 출력",fetchMovie())
+
+// 영화 정보 출력 - 비동기 함수로 출력
+fetchMovie().then(movie => {
+    console.log("영화정보 출력-비동기 함수", movie)
+}) 
+
+// 영화 정보 출력 - 즉시 실행함수로 출력
+; (async function () {
+    const movie = await fetchMovie()
+    console.log("영화정보 출력-즉시 실행함수", movie)
+})()
