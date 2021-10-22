b0633bb1d28831c898b33758f4d1f03c4d55b290
0andme <yu00mi97@gmail.com>
2021-10-23 03:00:03 +0900

    feat : finally 메소드 추가
    1. then().catch().finally()형태
    2. try{ }catch{ }finally{ } 형태
    
    finally 함수 실행시 콘솔에 메시지 출력

--- 영화정보/JS/5.finally.js
+++ 영화정보/JS/5.finally.js
@@ -1,20 +1,5 @@
-const url = "https://www.omdbapi.com/?i=tt3896198&apikey=7035c60c123"
-
-// 일반 함수로 fetchMovie() 선언
-function fetchMovie() {
-    return new Promise(async (resolve, reject) => {
-        const res = await fetch(url)
-        const data = await res.json()
-        if (data.Error) {
-            // reject(data)
-            // 혹은
-            reject(data.Error)
-        }
-        resolve(data)
-
-    })
-}
-
+// 1. finally 메소드 사용
+// 1-1. then-catch문 아래 코드로 변경
 fetchMovie()
     .then(movie => {
         console.log("영화정보 출력-비동기 함수", movie)
@@ -29,8 +14,7 @@ fetchMovie()
         console.log("finally-then-catch 결과")
 
     })
-
-
+// 1-2. async-await 문 아래 코드로 변경
 ;(async function () {
     try {
         const movie = await fetchMovie()
@@ -45,3 +29,8 @@ fetchMovie()
         console.log("finally-async-await 결과")
     }
 })()
+// then과 catch 중 하나가 실행되는 구조인데 finally메소드는
+// [then이 실행되든 catch가 실행되든][정상실행이든 오류발생이든] 무조건 실행되는 메소드이다.
+// 항상 실행되기 때문에 [로딩이 되든 안되든 무조건 실행해야하는] [로딩을 끄는] 코드를 넣는다 
+// finally는 비동기와 상관없다
+
