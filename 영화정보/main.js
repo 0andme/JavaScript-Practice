9424a87184b70ae6d270ed60e3c3c1517a921edd
0andme <yu00mi97@gmail.com>
2021-10-25 22:39:56 +0900

    feat : 이미지가 로드 되기 전과 후의 로딩요소 제어
    - 이미지로드를 확인하는 함수 loadImage import
    - 이미지 로드가 되면 사라질 로딩 요소 추가
    - 기존의 로딩요소와의 구분을 위해 키 el 의 값 변경
    -loadImage()함수와 then()함수를 이용하여 이미지 로드되면 로딩요소 stop

--- 영화정보/main.js
+++ 영화정보/main.js
@@ -1,56 +1,69 @@
-// Loader class 가져오기
-import { Loader } from "./utils/index.js"
+// Loader class, loadImage 함수 가져오기
+import {
+  Loader,
+  loadImage
+} from "./utils/index.js"
 
 const url = "https://www.omdbapi.com/?i=tt3896198&apikey=7035c60c"
 
 // 일반 함수로 fetchMovie() 선언
 function fetchMovie() {
-    return new Promise(async (resolve, reject) => {
-        const res = await fetch(url)
-        const data = await res.json()
-        if (data.Error) {
-            // reject(data)
-            // 혹은
-            reject(data.Error)
-        }
-        resolve(data)
+  return new Promise(async (resolve, reject) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    if (data.Error) {
+      // reject(data)
+      // 혹은
+      reject(data.Error)
+    }
+    resolve(data)
 
-    })
+  })
 }
 // loader 인스턴스 생성
 const loader = new Loader({
-    el: ".loading",
-    color:"red"
+  el: ".movie-loading",
+  color: "red"
 })
-// loader의 start메소드 실행 (화면에 로딩요소 출력)
 loader.start()
 
+// posterLoader 인스턴스 생성
+const posterLoader = new Loader({
+  el: ".poster-loading",
+  color: "green"
+})
+posterLoader.start()
+
 fetchMovie()
-    .then(movie => {
-        // movie 객체 출력
-        console.log("영화정보 출력-비동기 함수", movie)
-        // html 요소 찾기
-        const movieEl = document.querySelector(".movie")
-        const titleEl = movieEl.querySelector(".movie__title")
-        const posterEl = movieEl.querySelector(".movie__poster")
-        const posterImgEl = posterEl.querySelector("img")
-        // 요소에 내용 넣기
-        titleEl.textContent = movie.Title
-        posterImgEl.src = movie.Poster
+  .then(movie => {
+    // movie 객체 출력
+    console.log("영화정보 출력-비동기 함수", movie)
+    // html 요소 찾기
+    const movieEl = document.querySelector(".movie")
+    const titleEl = movieEl.querySelector(".movie__title")
+    const posterEl = movieEl.querySelector(".movie__poster")
+    const posterImgEl = posterEl.querySelector("img")
+    // 요소에 내용 넣기
+    titleEl.textContent = movie.Title
+    posterImgEl.src = movie.Poster
 
+    loadImage(movie.Poster).then(() => {
+      posterLoader.stop()
     })
-    .catch((errorMsg) => {
-        console.log(errorMsg)
-        const errEl = document.createElement('div')
-        errEl.textContent = errorMsg
-        document.body.append(errEl)
-    })
-    .finally(() => {
-      // 화면에 loader요소 사라짐
-      loader.stop()
-      console.log("finally-then-catch 결과")
 
-    })
+  })
+  .catch((errorMsg) => {
+    console.log(errorMsg)
+    const errEl = document.createElement('div')
+    errEl.textContent = errorMsg
+    document.body.append(errEl)
+  })
+  .finally(() => {
+    // 화면에 loader 요소 사라짐
+    loader.stop()
+    console.log("finally-then-catch 결과")
+
+  })
 
 
 // ;(async function () {
