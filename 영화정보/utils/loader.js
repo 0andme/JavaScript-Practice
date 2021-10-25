4b39d2c08c77cc64ccb0d250a72a0795960594cf
0andme <yu00mi97@gmail.com>
2021-10-25 17:06:31 +0900

    feat : style 요소 정의 추가 및 메소드 추가 styleEl요소의 정의 추가 start() stop() 메소드 정의

--- 영화정보/utils/loader.js
+++ 영화정보/utils/loader.js
@@ -8,12 +8,13 @@ export default class {
       color = "#333"
     } = options
     // html 요소 가져오기
-    this.loadingEl = document.querySelector(el)
-    this.loadingEl.classList.add("my-loader")
+    this.el = document.querySelector(el)
+    this.el.classList.add("my-loader")
     // style tag 생성 및 css 내용 추가
     const styleEl = document.createElement("style")
     styleEl.innerHTML =
       `.my-loader {
+        display: none;
         width: ${size}px;
         height: ${size}px;
         border-width: ${width}px;
@@ -23,6 +24,9 @@ export default class {
         border-top-color: transparent;
         animation: loading-spin .8s linear infinite;
       }
+      .my-loader.start {
+        display: block;
+      }
       @keyframes loading-spin {
         0% { transform: rotate(0deg); }
         100% { transform: rotate(360deg); }
@@ -30,5 +34,12 @@ export default class {
 
     //head tag에 styleEl 추가 
     document.head.append(styleEl)
+  }
+  // 프로토타입 메소드
+  start(){
+    this.el.classList.add("start")
+  }
+  stop(){
+    this.el.classList.remove("start")
   }
 }
