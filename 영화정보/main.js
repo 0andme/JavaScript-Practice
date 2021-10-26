712b07ea55be3b81eef8db340d37db34fe0c30e9
0andme <yu00mi97@gmail.com>
2021-10-26 15:15:49 +0900

    feat : fetch then 함수 주석 처리 및 async await 함수 작성
    - async await 함수 작성
    - 두 비동기 함수에 finally 코드 추가
    - 주석 추가

--- 영화정보/main.js
+++ 영화정보/main.js
@@ -34,8 +34,43 @@ const posterLoader = new Loader({
 })
 posterLoader.start()
 
-fetchMovie()
-  .then(movie => {
+// fetchMovie()
+//   .then(movie => {
+//     // movie 객체 출력
+//     console.log("영화정보 출력-비동기 함수", movie)
+//     // html 요소 찾기
+//     const movieEl = document.querySelector(".movie")
+//     const titleEl = movieEl.querySelector(".movie__title")
+//     const posterEl = movieEl.querySelector(".movie__poster")
+//     const posterImgEl = posterEl.querySelector("img")
+//     // 요소에 내용 넣기
+//     titleEl.textContent = movie.Title
+//     posterImgEl.src = movie.Poster
+//     // 이미지 로드 및 로딩요소 멈춤
+//     loadImage(movie.Poster).then(() => {
+//       posterLoader.stop()
+//     })
+
+//   })
+//   .catch((errorMsg) => {
+//     console.log(errorMsg)
+//     const errEl = document.createElement('div')
+//     errEl.textContent = errorMsg
+//     document.body.append(errEl)
+//   })
+//   .finally(() => {
+//     // 화면에 loader 요소 사라짐
+//     loader.stop()
+//     posterLoader.stop()
+//     console.log("finally-then-catch 결과")
+
+//   })
+
+
+;(async function () {
+  try {
+    const movie = await fetchMovie()
+    console.log("영화정보 출력-즉시 실행함수", movie)
     // movie 객체 출력
     console.log("영화정보 출력-비동기 함수", movie)
     // html 요소 찾기
@@ -46,37 +81,19 @@ fetchMovie()
     // 요소에 내용 넣기
     titleEl.textContent = movie.Title
     posterImgEl.src = movie.Poster
-
-    loadImage(movie.Poster).then(() => {
-      posterLoader.stop()
-    })
-
-  })
-  .catch((errorMsg) => {
+    // 이미지 로드 및 로딩요소 멈춤 
+    await loadImage(movie.Poster)
+    posterLoader.stop()
+  }
+  catch (errorMsg) {
     console.log(errorMsg)
     const errEl = document.createElement('div')
     errEl.textContent = errorMsg
     document.body.append(errEl)
-  })
-  .finally(() => {
-    // 화면에 loader 요소 사라짐
+  }
+  finally {
     loader.stop()
-    console.log("finally-then-catch 결과")
-
-  })
-
-
-// ;(async function () {
-//     try {
-//         const movie = await fetchMovie()
-//         console.log("영화정보 출력-즉시 실행함수", movie)
-//     } catch (errorMsg){
-//         console.log(errorMsg)
-//         const errEl = document.createElement('div')
-//         errEl.textContent = errorMsg
-//         document.body.append(errEl)
-
-//     }finally{
-//         console.log("finally-async-await 결과")
-//     }
-// })()
+    posterLoader.stop()
+    console.log("finally-async-await 결과")
+  }
+})()
